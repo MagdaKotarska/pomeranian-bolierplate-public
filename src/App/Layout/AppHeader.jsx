876d35477db4ae46f5b5ad773946cf4d79a1039e
Magdalena Kotarska <magdalena.kotarska85@gmail.com>
2023-04-27 15:54:27 +0200

    menu

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './styles/header.css';
 import { Logo } from '../Components/Logo/Logo';
+import { Link } from 'react-router-dom';
 
 export function AppHeader() {
   return (
     <header>
       <div>
-        <Logo />
+        <Link to="/">
+          <Logo />
+        </Link>
       </div>
     </header>
   );
